495bfddabff7917fa6ad46280acf55bce390655d
Marziah <marziah@newgen-bd.com>
2025-08-06 10:58:45 +0600

    prev code

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
-  basePath: isProd ? "/vc_fund" : "",
-  assetPrefix: isProd ? "/vc_fund/" : "",
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -11,3 +8,17 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const isProd = process.env.NODE_ENV === "production";
+// const nextConfig = {
+//   output: "export",
+//   basePath: isProd ? "/vc_fund" : "",
+//   assetPrefix: isProd ? "/vc_fund/" : "",
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   images: { unoptimized: true },
+// };
+
+// module.exports = nextConfig;

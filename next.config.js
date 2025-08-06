494ece400b715cb22fdae111fffc3a74ae4f166e
Marziah <marziah@newgen-bd.com>
2025-08-06 10:45:23 +0600

    configure update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-  output: 'export',
+  output: "export",
+  basePath: isProd ? "/vc_fund" : "",
+  assetPrefix: isProd ? "/vc_fund/" : "",
   eslint: {
     ignoreDuringBuilds: true,
   },
